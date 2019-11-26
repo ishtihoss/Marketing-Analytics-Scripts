874154803160d029b5bf723719bd5e09d325bce1
ishtihoss <ishtiaque.hossain@ada-asia.com>
2019-11-26 13:44:00 +0800

    changes made

--- Poll Evaluation Script.R
+++ Poll Evaluation Script.R
@@ -10,7 +10,7 @@ Marketing_in_2020_s_Economic_Landscape_1_191_ <- read_excel("C:/Users/IshtiaqueH
 # Clean the data
 
 m20 <- Marketing_in_2020_s_Economic_Landscape_1_191_ 
-names(m20) <- str_replace_all(names(m20),"[ ?]","")
+names(m20) <- str_replace_all(names(m20),"[ ?']","")
 
 # Question 5 Viz
 
@@ -22,6 +22,32 @@ summary_m20 %>% ggplot(aes(x=reorder(Areyoustrugglingtoacquiremorecustomersinthe
   scale_fill_distiller(palette = "Spectral", "% of respondents") + 
   geom_text(aes(label=round(Per_centage,2),hjust=-.01)) + 
   labs(x="",y="",title = "Are you struggling to acquire more customers in the current economic landscape?") + 
-  ggthemes::theme_wsj()
+  ggthemes::theme_economist()
+
+# Question 7 Viz
+
+summary_m20_q7 <- m20 %>% group_by(Doyouforeseehavingenoughbudgetforthemarketingactivitiesyoudliketorunin2020) %>% 
+  summarise(n=n()) %>% arrange(desc(n)) %>% mutate(Per_centage=n/sum(n)*100)
+
+summary_m20_q7 %>% ggplot(aes(x=reorder(Doyouforeseehavingenoughbudgetforthemarketingactivitiesyoudliketorunin2020,Per_centage),y=Per_centage,fill=Per_centage)) + 
+  geom_bar(stat="identity") + coord_flip() + 
+  scale_fill_distiller(palette = "Spectral", "% of respondents") + 
+  geom_text(aes(label=round(Per_centage,2),hjust=-.01)) + 
+  labs(x="",y="",title = "Do you foresee having enough budget for the marketing activities you'd like to run in 2020?
+") +   ggthemes::theme_economist()
+
+# Question 8 Viz
+
+summary_m20_q8 <- m20 %>% group_by(Whatdoyouthinktheeconomicgrowthforyourcountrywilllooklikein2020) %>% 
+  summarise(n=n()) %>% arrange(desc(n)) %>% mutate(Per_centage=n/sum(n)*100)
+
+summary_m20_q8 %>% ggplot(aes(x=reorder(Whatdoyouthinktheeconomicgrowthforyourcountrywilllooklikein2020,Per_centage),y=Per_centage,fill=Per_centage)) + 
+  geom_bar(stat="identity") + coord_flip() + 
+  scale_fill_distiller(palette = "Spectral", "% of respondents") + 
+  geom_text(aes(label=round(Per_centage,2),hjust=-.01)) + 
+  labs(x="",y="",title = "What do you think the economic growth for your country will look like in 2020?
+") +   ggthemes::theme_economist()
+
+# Industry Specific Slowing Growth
 
 
