00d233ec99973314af52ea1cb162b48dddd0a2c5
ishtibb <ishti@thebellbird.com>
2019-12-05 12:00:59 +0800

    changes

--- GA_script.R
+++ GA_script.R
@@ -22,6 +22,7 @@ google_analytics(ga_id,
 
 ga_data <- google_analytics(ga_id, 
                             date_range = c("2019-01-01", "2019-12-05"),
-                            metrics = c("users","newUsers","percentNewSessions","sessionsPerUser","sessions","bounces","bounceRate","sessionDuration","avgSessionDuration"),
-                            dimensions = c("userType","sessionCount","daysSinceLastSession","userDefinedValue","userBucket","sessionDurationBucket","referralPath","fullReferrer","campaign","source","medium","keyword","adContent","socialNetwork","hasSocialSourceReferral"),
+                            metrics = c("users","newUsers","percentNewSessions","sessionsPerUser","sessions","bounces","bounceRate","sessionDuration","avgSessionDuration","organicSearches","pageviewsPerSession","uniquePageviews","timeOnPage","avgTimeOnPage","exits","exitRate"),
+                            dimensions = c("userType","sessionCount","daysSinceLastSession","userDefinedValue","userBucket","sessionDurationBucket","referralPath","fullReferrer","campaign","source","medium","keyword","adContent","socialNetwork","hasSocialSourceReferral","browser","browserVersion","operatingSystem","operatingSystemVersion","mobileDeviceBranding","mobileDeviceModel","mobileInputSelector","mobileDeviceInfo","mobileDeviceMarketingName","deviceCategory","browserSize","dataSource","country","region"
+                            ,"metro","city","networkDomain","networkLocation","screenResolution","hostname","pagePath","pageTitle","landingPagePath","secondPagePath","exitPagePath","previousPagePath","pagePathLevel1","pagePathLevel2","pagePathLevel3","pagePathLevel4","pageDepth","userAgeBracket","userGender","interestOtherCategory","interestAffinityCategory","interestInMarketCategory","day","month","dayOfWeek"),
                             anti_sample = TRUE)
